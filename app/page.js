6520d86a856e4e3de5e0458300efffee742151b0
giovannaps <giovannapslima.gpsl@gmail.com>
2026-04-04 18:28:52 -0300

    adicionando drawer do cliente

--- app/page.js
+++ app/page.js
@@ -1,15 +1,24 @@
-import Header from "../components/Header"
-import HeroSection from "../components/HeroSection"
-import StatsSection from "../components/StatsSection"
-import ExperimentSection from "../components/ExperimentSection"
-import AboutSection from "../components/AboutSection"
-import TestimonialsSection from "../components/TestimonialsSection"
-import Footer from "../components/Footer"
+"use client";
+
+import { useState } from "react";
+
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import StatsSection from "../components/StatsSection";
+import ExperimentSection from "../components/ExperimentSection";
+import AboutSection from "../components/AboutSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import Footer from "../components/Footer";
+import Drawer from "../components/Drawer";
 
 export default function HomePage() {
+  const [open, setOpen] = useState(false);
+  const [client, setClient] = useState(null);
+
   return (
     <div className="min-h-screen bg-white">
       <Header />
+
       <main>
         <HeroSection />
         <StatsSection />
@@ -17,7 +26,14 @@ export default function HomePage() {
         <AboutSection />
         <TestimonialsSection />
       </main>
+
       <Footer />
+
+      <Drawer
+        open={open}
+        onClose={() => setOpen(false)}
+        client={client}
+      />
     </div>
-  )
+  );
 }
